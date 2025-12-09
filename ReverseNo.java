ff0c05f0604946b4a4f3b6cbf30afe32ed5b361b
Snehal Patil <141490358+patilssneha@users.noreply.github.com>
2025-12-09 17:17:49 +0530

    Add files via upload

---
+++ ReverseNo.java
@@ -0,0 +1,14 @@
+public class ReverseNo{
+    public static void main(String[] args) {
+               int num = 1234;
+                int reversed = 0;
+
+                while (num != 0) {
+                    int digit = num % 10;
+                    reversed = reversed * 10 + digit;
+                    num = num / 10;
+                }
+
+                System.out.println("Reversed number: " + reversed);
+            }
+        }
